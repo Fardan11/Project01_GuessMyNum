d26286c5c9682d419161929e0e0300d12a67fbe8
Fardan11 <fardankabir11@gmail.com>
2024-01-08 18:56:00 +0600

    'Finished'

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 
 //Dom is not part of JS
 
+// Refactoring is basically look for duplicates
+
 //to select class . (class selector()) , nothing for content
 
 /*
@@ -15,8 +17,9 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc to cut of fraction
+let secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc to cut of fraction
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); // doing this as we get string ,converting it to num;
@@ -32,6 +35,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
 
   // when guess is Higher
@@ -56,4 +63,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc to cut of fraction
+  score = 20;
+  highScore = 0;
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
